bb282ce8fb92321f6277d9cfe7abccb1389ccc24
李占盛 <774659715@qq.com>
2019-10-11 15:00:00 +0800

    1

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 
 ##资料
-spring 
-    https://spring.io/guides/
-    https://spring.io/guides/gs/serving-web-content/
-github登录api
-    https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+[spring 文档](https://spring.io/guides)
+[Spring Web](https://spring.io/guides/gs/serving-web-content)
+[ES 社区](https://elasticsearch.cn/explore)
+[GitHub deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+[Bootstrap](https://v3.bootcss.com/getting-started)
+[GitHub OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app)
+https://square.github.io/okhttp/
 
 ##工具
+[Git](https://git-scm.com/download)
+[Visual Paradigm](https://www.visual-paradigm.com)
---
+++ src/main/java/com/legendh/community/controller/AuthorizeController.java
@@ -0,0 +1,48 @@
+package com.legendh.community.controller;
+
+import com.legendh.community.dto.AccessTokenDTO;
+import com.legendh.community.dto.GithubUser;
+import com.legendh.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    /**
+     * GitHub登录-构造callback方法
+     *
+     * @param code
+     * @param state
+     * @return index
+     */
+    @GetMapping("callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
