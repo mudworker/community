53848ffb0a09896130779a574b7a8d202bfb3a96
李占盛 <774659715@qq.com>
2019-10-15 18:52:22 +0800

    add mysql insert

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 [GitHub deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
 [Bootstrap](https://v3.bootcss.com/getting-started)
 [GitHub OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app)
-https://square.github.io/okhttp/
+[okhttp](https://square.github.io/okhttp/)
+[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-container)
 
 ##工具
 [Git](https://git-scm.com/download)
--- src/main/java/com/legendh/community/controller/AuthorizeController.java
+++ src/main/java/com/legendh/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.legendh.community.controller;
 
 import com.legendh.community.dto.AccessTokenDTO;
 import com.legendh.community.dto.GithubUser;
+import com.legendh.community.mapper.UserMapper;
+import com.legendh.community.model.User;
 import com.legendh.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,6 +28,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * GitHub登录-构造callback方法
      *
@@ -33,7 +40,8 @@ public class AuthorizeController {
      */
     @GetMapping("callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) throws IOException {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -41,8 +49,21 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
